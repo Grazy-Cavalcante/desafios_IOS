33e48933f9cc0eb6154f98b3cc747dc36c22b393
Grazielly Cavalcante <grazioliveira2705@gmail.com>
2025-03-11 10:58:11 -0300

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,33 +1,29 @@
 //
 //  main.swift
-//  aula_um
+//  desafio_dois
 //
 //  Created by Aluno Mack on 11/03/25.
 //
 
 import Foundation
 
-print("Hello, World!")
-
-// Dado um número fixo, verifique se ele é par ou ímpar. Se o número for par, imprima "O número é par"e depois imprima todos os números de 1 até esse número. Caso o número seja impar, imprima "O número é impar"e depois imprima apenas os números ímpares de 1 até o número fornecido.
-
-print("Digite um número ")
-if let input = readLine(), let number = Int(input) {
-    if number % 2 == 0 {
-        print(" Esse número é par")
-        for i in 1...number {
-            if i % 2 == 0 { print (i)}
-        }
-        
-    }
-    else {
-        print("Esse número é impar")
-        for i in 1...number {
-            if i % 2 != 0 {
-                print(i)
+let multiplicador: Int = 1
+var fatorial: Int = 1
+print("Digite um número:")
+if let input = readLine(), let number = Int(input){
+    
+    for i in 1...number {
+        fatorial=fatorial*i
+        print ( fatorial)
+        if multiplicador == i  {
+            
+            if fatorial % 2 == 0{
+                print("Esse número é par")
             }
+            else {
+                print("Esse número é impar")
+            }
+            
         }
     }
-    
-    
 }
