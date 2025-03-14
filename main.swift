8349ae5a77ed7f6604e69e29d8383d5cbb04a0d8
Grazielly Cavalcante <grazioliveira2705@gmail.com>
2025-03-14 10:57:11 -0300

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,29 +1,55 @@
 //
 //  main.swift
-//  desafio_dois
+//  poo_desafio_tres
 //
-//  Created by Aluno Mack on 11/03/25.
+//  Created by Aluno Mack on 14/03/25.
 //
 
 import Foundation
 
-let multiplicador: Int = 1
-var fatorial: Int = 1
-print("Digite um número:")
-if let input = readLine(), let number = Int(input){
-    
-    for i in 1...number {
-        fatorial=fatorial*i
-        print ( fatorial)
-        if multiplicador == i  {
-            
-            if fatorial % 2 == 0{
-                print("Esse número é par")
-            }
-            else {
-                print("Esse número é impar")
-            }
-            
-        }
+// Desafio Três: Escreva uma classe que abstraia um quadrado. A classe deve ter um atributo que corresponde ao tamanho do lado e um método que retorne a área.//
+
+/*class quadrado {
+    var lado : Double
+    init(lado: Double) {
+        self.lado = lado
     }
+    func quadrado_um ()-> Double{
+        return lado*lado
+    }
+}
+let quadrado_um = quadrado(lado:2.5)
+
+print(quadrado_um.quadrado()) */
+
+
+//Desafio Dois: Escreva uma classe que abstraia um circulo. A classe deve ter um atributo que corresponde ao raio e dois métodos, um deve retornar o perímetro e o outro a área.
+
+/*class circulo {
+    var raio : Double
+    init(raio: Double) {
+        self.raio = raio
+    }
+    func area ()-> Double{
+        return (raio*raio)*3.14
+    }
+    func perimetro ()-> Double{
+        return raio*6.28
+    }
+}
+let circulo_um = circulo(raio:2.5)
+
+print (circulo_um.perimetro())
+print (circulo_um.area())*/
+
+// Desafio Um: Escreva uma classe que abstraia uma pirâmide. A classe deve ter dois atributos, um representa o lado da base e o outro a altura. Além disso, você deve escrever dois métodos, um deve retornar o volume e o outro a área superficial.
+
+class piramide{
+    var altura: Double
+    var lado : Double
+    init(altura: Double, lado: Double) {
+        self.altura = altura
+        self.lado = lado
+    }
+    
 }
